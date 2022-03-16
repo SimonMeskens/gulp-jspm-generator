c22a47967c094eaeffdce96c7ce0528d820be54d
simonmeskens <meskens.simon@gmail.com>
2022-03-16 07:50:52 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<p align="center">
+  <a href="https://gulpjs.com">
+    <img height="257" width="114" src="https://raw.githubusercontent.com/gulpjs/artwork/master/gulp-2x.png">
+  </a>
+  <p align="center">The streaming build system</p>
+</p>
+
+## JSPM Generator Gulp Plugin
+
+JSPM Generator Plugin for Gulp
+
+### Install
+
+```bash
+npm i -D gulp-jspm-generator
+```
+
+### Usage
+
+```js
+
+```
---
+++ index.js
@@ -0,0 +1,57 @@
+const transform = require("through2").obj;
+
+const PluginError = require("plugin-error");
+const PLUGIN_NAME = "gulp-jspm-generator";
+
+/**
+ * @author Simon Meskens
+ * @description JSPM Generator Plugin for Gulp
+ * @license MIT
+ *
+ * @module gulp-jspm-generator
+ *
+ * @requires plugin-error
+ * @requires through2
+ *
+ * @method gulp-jspm-generator
+ *
+ * @typedef {import('@jspm/generator').Generator} Generator
+ *
+ * @param  {Generator} generator JSPM Generator
+ * @param  {Object}    options   Options
+ *
+ * @return {Function}            Stream (Transform)
+ */
+module.exports = function importmap(generator, options = {}) {
+  return transform((file, _, cb) => {
+    if (file.isNull()) {
+      return cb(null, file);
+    }
+
+    if (file.isStream()) {
+      return cb(
+        new PluginError({
+          plugin: PLUGIN_NAME,
+          message: "Streams are not supported",
+        })
+      );
+    }
+
+    return generator
+      .htmlGenerate(file.contents.toString(), options)
+      .then((html) => {
+        file.contents = Buffer.from(html);
+        cb(null, file);
+      })
+      .catch((err) => {
+        cb(
+          new PluginError({
+            plugin: PLUGIN_NAME,
+            message: err.message,
+            stack: err.stack,
+            showStack: true,
+          })
+        );
+      });
+  });
+};
